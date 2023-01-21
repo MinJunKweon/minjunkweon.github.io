18262fe4208e4565b4a0fb407c53de503c293797
MinJun Kweon <minz.kweon@gmail.com>
2023-01-22 00:35:00 +0900

    fix: OpenGrapgh Tag attributes

--- src/components/Meta/Meta.tsx
+++ src/components/Meta/Meta.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 
+import { graphql, StaticQuery } from "gatsby";
+
 interface Props {
   description: string;
   image?: string;
   title: string;
+}
+
+interface Data {
+  images: {
+    edges: Array<{
+      node: {
+        publicURL: string;
+      };
+    }>;
+  };
 }
 
 const Meta: React.FC<Props> = ({ description, title, image }: Props) => (
@@ -13,17 +25,46 @@ const Meta: React.FC<Props> = ({ description, title, image }: Props) => (
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:title" content={title} />
     <meta name="twitter:description" content={description} />
-    <meta name="og:title" content={title} />
-    <meta name="og:type" content="website" />
-    <meta name="og:description" content={description} />
+    <meta property="og:title" content={title} />
+    <meta property="og:type" content="website" />
+    <meta property="og:description" content={description} />
+    <meta property="og:site_name" content={title} />
+    <meta property="og:url" content="https://minz.dev" />
+    <meta property="og:article:author" content="MinJun Kweon" />
 
     {image ? (
       <>
-        <meta name="image" content={image} />
-        <meta name="og:image" content={image} />
-        <meta name="twitter:image" content={image} />
+        <meta property="image" content={image} />
+        <meta property="og:image" content={image} />
+        <meta property="twitter:image" content={image} />
       </>
-    ) : null}
+    ) : (
+      <StaticQuery
+        query={graphql`
+          query {
+            images: allFile(
+              filter: { relativePath: { eq: "og-image.png" } }
+            ) {
+              edges {
+                node {
+                  publicURL
+                }
+              }
+            }
+          }
+        `}
+        render={(data: Data) => {
+          const { images: { edges = [] } = {} } = data;
+          const { publicURL } = edges[0].node;
+          
+          return <>
+            <meta property="image" content={publicURL} />
+            <meta property="og:image" content={publicURL} />
+            <meta property="twitter:image" content={publicURL} />
+          </>;
+        }}
+      />
+    )}
   </>
 );
 
